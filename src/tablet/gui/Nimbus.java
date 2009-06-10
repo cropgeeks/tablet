737b130370628130556df534c3314dac38a030ea
Iain Milne <imilne@users.noreply.github.com>
2009-06-10 08:26:53 +0000

    - NEW: Nimbus will now be the default LaF for all non-Apple systems. - NEW: Added a class for handling Nimbus functionality (as it's probably going to be a pain). - CHG: Removed the jar file for the Office LaF.

---
+++ src/tablet/gui/Nimbus.java
@@ -0,0 +1,37 @@
+package tablet.gui;
+
+import java.awt.*;
+import javax.swing.*;
+
+class Nimbus
+{
+	private static final Color BACKGROUND = new Color(240, 240, 240);
+	private static final Font FONT = new Font("Tahoma", Font.PLAIN, 11);
+
+	static void customizeNimbus()
+		throws Exception
+	{
+//		UIManager.put("nimbusBase", BACKGROUND);
+//		UIManager.put("nimbusBlueGrey", BACKGROUND);
+		UIManager.put("control", BACKGROUND);
+
+		UIManager.put("defaultFont", FONT);
+		UIManager.put("Label[Enabled].font", FONT);
+		UIManager.put("Table[Enabled].font", FONT);
+		UIManager.put("TableHeader[Enabled].font", FONT);
+		UIManager.put("TabbedPane[Enabled].font", FONT);
+
+
+		for (UIManager.LookAndFeelInfo laf : UIManager.getInstalledLookAndFeels())
+			if (laf.getName().equals("Nimbus"))
+				UIManager.setLookAndFeel(laf.getClassName());
+
+
+
+		UIManager.put("SplitPane[Enabled].size", 8);
+
+		UIManager.put("nimbusOrange", new Color(51, 98, 140));
+//		UIManager.put("nimbusOrange", new Color(57, 105, 138));
+//		UIManager.put("nimbusOrange", new Color(115, 164, 209));
+	}
+}
