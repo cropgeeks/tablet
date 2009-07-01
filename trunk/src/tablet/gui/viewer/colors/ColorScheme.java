d3bd51d1cca22481a4cdaa0b98ee52fb5881ebd7
imilne <imilne@d454dee0-156b-47da-bcea-d5989f25f5ea>
2009-07-01 08:34:37 +0000

    - NEW: The colour schemes are now cached so each one (scheme and resolution) only needs to be created once. It doesn't seem to use too much memory: ~4MB if every scheme and size is held.

--- trunk/src/tablet/gui/viewer/colors/ColorScheme.java
+++ trunk/src/tablet/gui/viewer/colors/ColorScheme.java
@@ -2,6 +2,7 @@ package tablet.gui.viewer.colors;
 
 import java.awt.*;
 import java.awt.image.*;
+import java.util.*;
 
 import tablet.data.*;
 
@@ -9,6 +10,10 @@ public abstract class ColorScheme
 {
 	public static final int STANDARD = 10;
 	public static final int TEXT = 20;
+
+	// Maintains a collection of every colour scheme created so far
+	private static Hashtable<String, ColorScheme> schemes =
+		new Hashtable<String, ColorScheme>();
 
 	protected Contig contig;
 
@@ -22,4 +27,61 @@ public abstract class ColorScheme
 	public abstract BufferedImage getConsensusImage(int data);
 
 	public abstract Color getColor(int data);
+
+
+	/** Returns a DNA colouring scheme from the cache of schemes. */
+	public static ColorScheme getDNA(int type, Contig contig, int w, int h)
+	{
+		ColorScheme scheme = null;
+
+		if (type == STANDARD)
+		{
+			scheme = schemes.get("STDDNA" + w);
+			if (scheme == null)
+			{
+				scheme = new StandardColorScheme(contig, w, h);
+				schemes.put("STDDNA" + w, scheme);
+			}
+		}
+
+		else
+		{
+			scheme = schemes.get("TXTDNA" + w);
+			if (scheme == null)
+			{
+				scheme = new TextColorScheme(contig, w, h);
+				schemes.put("TXTDNA" + w, scheme);
+			}
+		}
+
+		return scheme;
+	}
+
+	/** Returns a PROTEIN colouring scheme from the cache of schemes. */
+	public static ColorScheme getProtein(int type, Contig contig, int w, int h)
+	{
+		ColorScheme scheme = null;
+
+		if (type == STANDARD)
+		{
+			scheme = schemes.get("STDPRO" + w);
+			if (scheme == null)
+			{
+				scheme = new ProteinClassificationColorScheme(contig, w, h);
+				schemes.put("STDPRO" + w, scheme);
+			}
+		}
+
+		else
+		{
+			scheme = schemes.get("TXTPRO" + w);
+			if (scheme == null)
+			{
+				scheme = new ProteinTextColorScheme(contig, w, h);
+				schemes.put("TXTPRO" + w, scheme);
+			}
+		}
+
+		return scheme;
+	}
 }
