82a2649d94e8f529fdb9105d78fe04bbc924bc31
Iain Milne <imilne@users.noreply.github.com>
2009-07-02 08:29:24 +0000

    - NEW: Copy to clipboard for protein translations has been implemented. - NEW: The code for formatting sequences into FASTA has been moved into TabletUtils.

--- src/tablet/gui/TabletUtils.java
+++ src/tablet/gui/TabletUtils.java
@@ -15,4 +15,25 @@ public class TabletUtils
 
 		return p1;
 	}
+
+	/** Produces a FASTA formatted version of a sequence. */
+	public static String formatFASTA(String title, String sequence)
+	{
+		String lb = System.getProperty("line.separator");
+
+		StringBuffer text = new StringBuffer(sequence.length());
+		text.append(">" + title + lb);
+
+		for (int i = 0; i < sequence.length(); i += 50)
+		{
+			try	{
+				text.append(sequence.substring(i, i+50) + lb);
+			}
+			catch (Exception e)	{
+				text.append(sequence.substring(i, sequence.length()) + lb);
+			}
+		}
+
+		return text.toString();
+	}
 }
