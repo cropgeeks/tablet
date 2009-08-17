9cd13408bb39703e0df182bd4a633ec88e8ba21a
Gordon Stephen <12711372+gordonstephen@users.noreply.github.com>
2009-08-17 14:25:08 +0000

    - NEW: Method to get the filename of a file given a filepath / browse for a new file.

--- src/tablet/gui/TabletUtils.java
+++ src/tablet/gui/TabletUtils.java
@@ -1,6 +1,7 @@
 package tablet.gui;
 
 import java.awt.*;
+import java.io.File;
 import java.net.*;
 import javax.swing.*;
 
@@ -8,6 +9,8 @@ import scri.commons.gui.*;
 
 public class TabletUtils
 {
+	public static boolean dirChanged = false;
+
 	public static JPanel getButtonPanel()
 	{
 		JPanel p1 = new JPanel(new FlowLayout(FlowLayout.RIGHT, 5, 0));
@@ -54,6 +57,43 @@ public class TabletUtils
 			TaskDialog.error(
 				RB.format("gui.TabletUtils.urlError", html),
 				RB.getString("gui.text.close"));
+		}
+	}
+
+	public static String getFilename(String title, String filePath)
+	{
+		WinMain winMain = Tablet.winMain;
+		// Decide on AWT or Swing dialog based on OS X or not
+		if (SystemUtils.isMacOS() == false)
+		{
+			JFileChooser fc = new JFileChooser();
+			fc.setDialogTitle(title);
+			if(dirChanged)
+			    fc.setCurrentDirectory(new File(Prefs.guiCurrentDir));
+			else
+			    fc.setCurrentDirectory(new File(filePath));
+
+			if (fc.showOpenDialog(winMain) != JFileChooser.APPROVE_OPTION)
+				return null;
+
+			Prefs.guiCurrentDir = fc.getCurrentDirectory().getPath();
+			return fc.getSelectedFile().getPath();
+		}
+		else
+		{
+			FileDialog fd = new FileDialog(winMain, title, FileDialog.LOAD);
+			if(dirChanged)
+			    fd.setDirectory(Prefs.guiCurrentDir);
+			else
+			    fd.setDirectory(filePath);
+			fd.setLocationRelativeTo(winMain);
+			fd.setVisible(true);
+
+			if (fd.getFile() == null)
+				return null;
+
+			Prefs.guiCurrentDir = fd.getDirectory();
+			return new File(fd.getDirectory(), fd.getFile()).getPath();
 		}
 	}
 }
