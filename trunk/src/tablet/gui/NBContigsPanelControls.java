16dfa13bc86d72e38538477de820f56fb889fb33
gsteph <gsteph@d454dee0-156b-47da-bcea-d5989f25f5ea>
2010-02-24 14:50:13 +0000

    - BUG: Fixed bug which caused Tablet to not exit correctly when a filter had been applied in the ContigsPanel.

--- trunk/src/tablet/gui/NBContigsPanelControls.java
+++ trunk/src/tablet/gui/NBContigsPanelControls.java
@@ -128,13 +128,13 @@ class NBContigsPanelControls extends JPanel implements ActionListener, DocumentL
 				float number = nf.parse(textField.getText()).floatValue();
 				rf = RowFilter.numberFilter(ComparisonType.BEFORE, number+1, 4);
 			}
+			
+			panel.setTableFilter(rf);
 		}
 		catch (Exception e)
 		{
 			System.out.println(e);
 		}
-
-		panel.setTableFilter(rf);
 	}
 
 	void setEnabledState(boolean state)
