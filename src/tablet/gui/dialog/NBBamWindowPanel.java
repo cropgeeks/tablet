e393f1fb91b3de0d0d7dfa043bef5dad16f91e22
Iain Milne <imilne@users.noreply.github.com>
2010-05-05 10:51:13 +0000

    - NEW: Made some visual modifications to the Adjust BAM Window Size dialog. - NEW: Added the www help content for the dialog too.

--- src/tablet/gui/dialog/NBBamWindowPanel.java
+++ src/tablet/gui/dialog/NBBamWindowPanel.java
@@ -14,7 +14,6 @@ class NBBamWindowPanel extends JPanel
 		initComponents();
 
 		setBackground(Color.white);
-		panel.setBackground(Color.white);
 		setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
 
 		RB.setText(bamLabel, "gui.dialog.NBBamWindowPanel.bamLabel");
@@ -33,40 +32,37 @@ class NBBamWindowPanel extends JPanel
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        panel = new javax.swing.JPanel();
-        bamSpinner = new javax.swing.JSpinner();
         bamLabel = new javax.swing.JLabel();
+        bamSpinner = new javax.swing.JSpinner();
         label1 = new javax.swing.JLabel();
         label2 = new javax.swing.JLabel();
 
-        panel.setBorder(javax.swing.BorderFactory.createTitledBorder("Adjust:"));
+        bamLabel.setText("BAM data window size (in base pairs):");
 
-        bamLabel.setText("BAM data window size (in bp):");
+        label1.setText("A large window allows you to see more data at once, at the expense of load");
 
-        label1.setText("A large window allows you to see more data at once, at the");
+        label2.setText("time and memory usage.");
 
-        label2.setText("expense of load time and memory usage.");
-
-        javax.swing.GroupLayout panelLayout = new javax.swing.GroupLayout(panel);
-        panel.setLayout(panelLayout);
-        panelLayout.setHorizontalGroup(
-            panelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(panelLayout.createSequentialGroup()
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(panelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(panelLayout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
                         .addComponent(bamLabel)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(bamSpinner, javax.swing.GroupLayout.DEFAULT_SIZE, 132, Short.MAX_VALUE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(bamSpinner, javax.swing.GroupLayout.DEFAULT_SIZE, 181, Short.MAX_VALUE))
                     .addComponent(label1)
                     .addComponent(label2))
                 .addContainerGap())
         );
-        panelLayout.setVerticalGroup(
-            panelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(panelLayout.createSequentialGroup()
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(panelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(bamLabel)
                     .addComponent(bamSpinner, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
@@ -75,32 +71,14 @@ class NBBamWindowPanel extends JPanel
                 .addComponent(label2)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
-
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
-        this.setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(panel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addContainerGap())
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(panel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addContainerGap())
-        );
     }// </editor-fold>//GEN-END:initComponents
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel bamLabel;
-    javax.swing.JSpinner bamSpinner;
+    private javax.swing.JSpinner bamSpinner;
     private javax.swing.JLabel label1;
     private javax.swing.JLabel label2;
-    private javax.swing.JPanel panel;
     // End of variables declaration//GEN-END:variables
 
 }
