d3516b39abbdacee0ff14bee382ceca8cb1cd496
imilne <imilne@d454dee0-156b-47da-bcea-d5989f25f5ea>
2010-07-06 10:50:54 +0000

    - NEW: Major changes to the installer to cope with install4j 5. - CHG: Changed the sqlite jars to custom edits from www.xerial.org for each platform. Still need to work out licensing.

--- trunk/j.bat
+++ trunk/j.bat
@@ -1,4 +1,6 @@
 @ECHO OFF
 CALL libraries.bat
 
-java -Xmx1024m -Djava.library.path=lib\sqlite -cp .;classes;%avcp% tablet.gui.Tablet %1 %2 %3 %4 %5
+SET avcp=%avcp%;%lib%\sqlite-jdbc-3.6.19-win32.jar
+
+java -Xmx1024m -cp .;classes;%avcp% tablet.gui.Tablet %1 %2 %3 %4 %5
