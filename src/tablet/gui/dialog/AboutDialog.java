1a099b94ad81e94f22722613323c5dc449083684
Iain Milne <imilne@users.noreply.github.com>
2011-08-11 13:34:35 +0000

    - NEW: Changed the copyright labels to show ICS/JHI information rather than SCRI.

--- src/tablet/gui/dialog/AboutDialog.java
+++ src/tablet/gui/dialog/AboutDialog.java
@@ -31,7 +31,7 @@ public class AboutDialog extends JDialog implements ActionListener
 
 		JTabbedPane tabs = new JTabbedPane();
 		tabs.add(RB.getString("gui.dialog.AboutDialog.tab1"), nbPanel);
-		tabs.add(RB.getString("gui.dialog.AboutDialog.tab2"), avatars);
+		tabs.add(RB.format("gui.dialog.AboutDialog.tab2", "\u0026"), avatars);
 
 		add(tabs);
 		add(createButtons(), BorderLayout.SOUTH);
