2b2d465f1c2dd7aef5c928c92f314ee41641c7dd
imilne <imilne@d454dee0-156b-47da-bcea-d5989f25f5ea>
2011-10-13 14:51:33 +0000

    - BUG: Fixed a problem with long file paths causing problems with the combo boxes on OS X.

--- trunk/src/tablet/gui/FindPanelNB.java
+++ trunk/src/tablet/gui/FindPanelNB.java
@@ -103,7 +103,6 @@ public class FindPanelNB extends javax.swing.JPanel
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        findCombo = new scri.commons.gui.matisse.HistoryComboBox();
         resultsLabel = new javax.swing.JLabel();
         bFind = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
@@ -118,6 +117,8 @@ public class FindPanelNB extends javax.swing.JPanel
         label = new javax.swing.JLabel();
         checkIgnorePads = new javax.swing.JCheckBox();
         findInCheckBox = new javax.swing.JCheckBox();
+        jPanel1 = new javax.swing.JPanel();
+        findCombo = new scri.commons.gui.matisse.HistoryComboBox();
 
         resultsLabel.setText("Results:");
 
@@ -144,6 +145,10 @@ public class FindPanelNB extends javax.swing.JPanel
 
         findInCheckBox.setText("Search in current contig only");
 
+        jPanel1.setLayout(new java.awt.GridLayout());
+
+        jPanel1.add(findCombo);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
@@ -154,18 +159,18 @@ public class FindPanelNB extends javax.swing.JPanel
                     .addComponent(findInCheckBox)
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(label)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 18, Short.MAX_VALUE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 38, Short.MAX_VALUE)
                         .addComponent(helpLabel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addComponent(checkUseRegex, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(findCombo, javax.swing.GroupLayout.DEFAULT_SIZE, 129, Short.MAX_VALUE)
+                    .addComponent(checkUseRegex, javax.swing.GroupLayout.DEFAULT_SIZE, 187, Short.MAX_VALUE)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 149, Short.MAX_VALUE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(bFind, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addComponent(searchTypeCombo, 0, 167, Short.MAX_VALUE)
+                    .addComponent(searchTypeCombo, 0, 187, Short.MAX_VALUE)
                     .addComponent(checkIgnorePads)
                     .addComponent(resultsLabel))
                 .addContainerGap())
-            .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 187, Short.MAX_VALUE)
+            .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 207, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -175,10 +180,10 @@ public class FindPanelNB extends javax.swing.JPanel
                     .addComponent(label)
                     .addComponent(helpLabel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(findCombo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(bFind))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(bFind, 0, 0, Short.MAX_VALUE)
+                    .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(9, 9, 9)
                 .addComponent(searchTypeCombo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(8, 8, 8)
                 .addComponent(findInCheckBox)
@@ -191,9 +196,6 @@ public class FindPanelNB extends javax.swing.JPanel
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 47, Short.MAX_VALUE))
         );
-
-        layout.linkSize(javax.swing.SwingConstants.VERTICAL, new java.awt.Component[] {bFind, findCombo});
-
     }// </editor-fold>//GEN-END:initComponents
 
 
@@ -204,6 +206,7 @@ public class FindPanelNB extends javax.swing.JPanel
     public scri.commons.gui.matisse.HistoryComboBox findCombo;
     javax.swing.JCheckBox findInCheckBox;
     scri.commons.gui.matisse.HyperLinkLabel helpLabel;
+    private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JLabel label;
     public javax.swing.JLabel resultsLabel;
