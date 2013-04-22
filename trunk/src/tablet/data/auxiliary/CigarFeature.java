3109af252ec234a3334ff4044b3ec66c9c016042
imilne <imilne@d454dee0-156b-47da-bcea-d5989f25f5ea>
2013-04-22 08:31:44 +0000

    - NEW: Updated the web files ready for a new release. - NEW: Updated all the licence information and copyright headers for 2013.

--- trunk/src/tablet/data/auxiliary/CigarFeature.java
+++ trunk/src/tablet/data/auxiliary/CigarFeature.java
@@ -1,3 +1,6 @@
+// Copyright 2009-2013 Information & Computational Sciences, JHI. All rights
+// reserved. Use is subject to the accompanying licence terms.
+
 package tablet.data.auxiliary;
 
 import java.util.*;
