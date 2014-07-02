0f3077680d223114aeddb34a8c5aa9d7ecf505f1
gsteph <gsteph@d454dee0-156b-47da-bcea-d5989f25f5ea>
2014-07-02 09:08:35 +0000

    - BUG: History snapshot now respects packing mode changes.

--- trunk/src/tablet/gui/viewer/SnapshotGrabber.java
+++ trunk/src/tablet/gui/viewer/SnapshotGrabber.java
@@ -51,22 +51,33 @@ public class SnapshotGrabber extends Thread
 		int colIndex = rCanvas.contig.getVisualStart() + (int)(rCanvas.pX1 / rCanvas.ntW);
 		int rowIndex = rCanvas.pY1 / rCanvas.ntH;
 
-		Snapshot snapshot = new Snapshot(colIndex, rowIndex, Prefs.visReadsZoomLevel);
+		Snapshot snapshot = new Snapshot(colIndex, rowIndex, Prefs.visReadsZoomLevel, Prefs.visPacked, Prefs.visPaired);
 
 		controller.addSnapshot(snapshot);
 	}
 
+	/**
+	 * A snapshot captures all of the information required to recreate a view
+	 * of data within the current Contig. As such it stores the x and y
+	 * coordinates of the display, the zoom level and current packing mode (due
+	 * to historical reasons two booleans indicating if the view is packed and
+	 * if the view is paired).
+	 */
 	public class Snapshot
 	{
 		private int x;
 		private int y;
 		private int zoom;
+		private boolean packed;
+		private boolean paired;
 
-		Snapshot(int x, int y, int zoom)
+		Snapshot(int x, int y, int zoom, boolean packed, boolean paired)
 		{
 			this.x = x;
 			this.y = y;
 			this.zoom = zoom;
+			this.packed = packed;
+			this.paired = paired;
 		}
 
 		public int getX()
@@ -77,6 +88,12 @@ public class SnapshotGrabber extends Thread
 
 		public int getZoom()
 			{ return zoom; }
+
+		public boolean getPacked()
+			{ return packed; }
+
+		public boolean getPaired()
+			{ return paired; }
 
 		@Override
 		public boolean equals(Object other)
@@ -89,9 +106,10 @@ public class SnapshotGrabber extends Thread
 
 			Snapshot o = (Snapshot)other;
 
-			return this.x == o.x && this.y == o.y && this.zoom == o.zoom;
+			return this.x == o.x && this.y == o.y && this.zoom == o.zoom && this.packed == o.packed && this.paired == o.paired;
 		}
 
+		@Override
 		public String toString()
 		{
 			return "x: " + x + " zoom: " + zoom;
