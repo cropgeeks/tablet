223084e4902368085b2e5adca20bd5df6da32980
Iain Milne <imilne@users.noreply.github.com>
2014-07-18 10:33:43 +0000

    - NEW: Updated the licence information for 2014.

--- src/tablet/io/GtfReader.java
+++ src/tablet/io/GtfReader.java
@@ -1,3 +1,6 @@
+// Copyright 2009-2014 Information & Computational Sciences, JHI. All rights
+// reserved. Use is subject to the accompanying licence terms.
+
 package tablet.io;
 
 import java.io.*;
