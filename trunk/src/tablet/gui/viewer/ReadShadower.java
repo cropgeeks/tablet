f748de2ac2d03ad4ce5b8afc321dfdc6d3a4975c
gsteph <gsteph@d454dee0-156b-47da-bcea-d5989f25f5ea>
2015-08-28 09:15:14 +0000

    - NEW: The ReadShadower now also shaddows reads pairs as well as the reads under the mouse.

--- trunk/src/tablet/gui/viewer/ReadShadower.java
+++ trunk/src/tablet/gui/viewer/ReadShadower.java
@@ -3,19 +3,21 @@
 
 package tablet.gui.viewer;
 
-import java.awt.Color;
-import java.awt.Graphics2D;
+import java.awt.*;
 
-import tablet.data.Read;
-import tablet.gui.Prefs;
+import tablet.data.*;
+import tablet.gui.*;
 
 class ReadShadower implements IOverlayRenderer
 {
-	private int overlayOpacity = 75;
-	private AssemblyPanel aPanel;
-	private ReadsCanvas rCanvas;
+	private final int overlayOpacity = 75;
+	private final AssemblyPanel aPanel;
+	private final ReadsCanvas rCanvas;
 
-	private Color lineColor = new Color(25, 75, 100);
+	private final Color lineColor = new Color(25, 75, 100);
+
+	private final Color shadowColor = new Color(0, 0, 0, overlayOpacity);
+	private final Color mateShadowColor = new Color(255, 0, 0, overlayOpacity);
 
 	static Integer mouseBase;
 
@@ -28,43 +30,41 @@ class ReadShadower implements IOverlayRenderer
 	/**
 	 * Draws the read shadowing on top of the reads canvas.
 	 */
+	@Override
 	public void render(Graphics2D g)
 	{
 		if (Prefs.visReadShadowing == 0)
 			return;
 
-		g.setPaint(new Color(0, 0, 0, overlayOpacity));
+		g.setPaint(shadowColor);
+
+		int ntH = rCanvas.ntH;
+		int yStart = rCanvas.pY1 / ntH;
+		int yEnd = rCanvas.pY2 / ntH;
 
 		if (Prefs.visReadShadowing == 1)
-			renderLockedToMiddle(g);
+			renderLockedToMiddle(g, yStart, yEnd);
 
 		else
-			renderFreeFlowing(g);
+			renderFreeFlowing(g, yStart, yEnd);
 	}
 
 	/**
 	 * Renders the read shadowing with respect to the centre of the reads canvas.
 	 * This is represented by a line down the middle of the reads canvas.
 	 */
-	private void renderLockedToMiddle(Graphics2D g)
+	private void renderLockedToMiddle(Graphics2D g, int yStart, int yEnd)
 	{
 		int midPointPixel = (rCanvas.pX1 + ((rCanvas.pX2Max-rCanvas.pX1)/2));
-		int top = rCanvas.pY1 / rCanvas.ntH;
-		int bottom = rCanvas.pY2 / rCanvas.ntH;
-		int ntH = rCanvas.ntH;
 
-		for (int row = top; row <= bottom; row++)
+		int base = rCanvas.getBaseForPixel(midPointPixel);
+
+		for (int row = yStart; row <= yEnd; row++)
 		{
-			int base = rCanvas.getBaseForPixel(midPointPixel);
 			Read read = rCanvas.reads.getReadAt(row, base);
 
-			if (read != null && read.isNotMateLink())
-			{
-				int xS = rCanvas.getFirstRenderedPixel(read.s());
-				int xE = rCanvas.getFinalRenderedPixel(read.e());
-
-				g.fillRect(xS, row * ntH, xE-xS+1, rCanvas.readH);
-			}
+			shadowRead(g, read, row);
+			shadowMate(g, read);
 		}
 		// Draws a vertical line down the middle of the display
 		g.setColor(lineColor);
@@ -75,7 +75,7 @@ class ReadShadower implements IOverlayRenderer
 	 * Draws the read shadowing relative to the base the mouse is currently over,
 	 * or a base that the shadowing has been locked to.
 	 */
-	private void renderFreeFlowing(Graphics2D g)
+	private void renderFreeFlowing(Graphics2D g, int yStart, int yEnd)
 	{
 		// Start by setting the position equal to the locked base
 		Integer iPosition = aPanel.getVisualContig().getLockedBase();
@@ -87,21 +87,12 @@ class ReadShadower implements IOverlayRenderer
 		if (iPosition == null)
 			return;
 
-		int ntH = rCanvas.ntH;
-		int yS = rCanvas.pY1 / ntH;
-		int yE = rCanvas.pY2 / ntH;
-
-		for (int row = yS; row <= yE; row++)
+		for (int row = yStart; row <= yEnd; row++)
 		{
 			Read read = rCanvas.reads.getReadAt(row, iPosition);
 
-			if (read != null && read.isNotMateLink())
-			{
-				int xS = rCanvas.getFirstRenderedPixel(read.s());
-				int xE = rCanvas.getFinalRenderedPixel(read.e());
-
-				g.fillRect(xS, row * ntH, xE-xS+1, rCanvas.readH);
-			}
+			shadowRead(g, read, row);
+			shadowMate(g, read);
 		}
 		// Draws a vertical line down the display
 		g.setColor(lineColor);
@@ -110,6 +101,36 @@ class ReadShadower implements IOverlayRenderer
 			+ (int) rCanvas.ntW / 2;
 
 		g.drawLine(linePos, rCanvas.pY1, linePos, rCanvas.pY2);
+	}
+
+	private void shadowRead(Graphics2D g, Read read, int row)
+	{
+		if (read != null && read.isNotMateLink())
+		{
+			int xS = rCanvas.getFirstRenderedPixel(read.s());
+			int xE = rCanvas.getFinalRenderedPixel(read.e());
+
+			g.fillRect(xS, row * rCanvas.ntH, xE-xS+1, rCanvas.readH);
+		}
+	}
+
+	private void shadowMate(Graphics2D g, Read read)
+	{
+		// Set the colour to the pair highlight color
+		g.setColor(mateShadowColor);
+
+		if (read != null && read instanceof MatedRead)
+		{
+			MatedRead mRead = (MatedRead)read;
+			Read mate = mRead.getMate();
+			if (mate != null)
+			{
+				int line = rCanvas.reads.getLineForRead(mate);
+				shadowRead(g, mate, line);
+			}
+		}
+
+		g.setColor(shadowColor);
 	}
 
 	public static void setMouseBase(Integer newBase)
