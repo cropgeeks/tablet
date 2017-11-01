fce065f5d8d8c4b329168d0885ee1254fe22d7bf
imilne <imilne@d454dee0-156b-47da-bcea-d5989f25f5ea>
2017-11-01 09:27:26 +0000

    - NEW: Added a specific class for performing Bioconda version checks.

---
+++ trunk/src/tablet/gui/TabletVersion.java
@@ -0,0 +1,13 @@
+// Copyright 2009-2017 Information & Computational Sciences, JHI. All rights
+// reserved. Use is subject to the accompanying licence terms.
+
+package tablet.gui;
+
+public class TabletVersion
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Tablet 1.17.08.17");
+		System.exit(0);
+	}
+}
