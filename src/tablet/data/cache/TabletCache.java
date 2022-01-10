7cad80e9c3e3c6f7f3ba4ef09eef16c00e6cad09
Iain Milne <imilne@users.noreply.github.com>
2022-01-10 13:29:20 +0000

    - CHG: Changing licence headers for 2022.

--- src/tablet/data/cache/TabletCache.java
+++ src/tablet/data/cache/TabletCache.java
@@ -1,4 +1,4 @@
-// Copyright 2009-2021 Information & Computational Sciences, JHI. All rights
+// Copyright 2009-2022 Information & Computational Sciences, JHI. All rights
 // reserved. Use is subject to the accompanying licence terms.
 
 package tablet.data.cache;
