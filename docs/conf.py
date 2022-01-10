702695b8f7c38868b31d37c748df9ca9abe6caaf
Iain Milne <imilne@users.noreply.github.com>
2022-01-10 13:29:46 +0000

    - CHG: Changing licence headers for 2022.

--- docs/conf.py
+++ docs/conf.py
@@ -45,7 +45,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Tablet'
-copyright = u'2021, Information & Computational Sciences, The James Hutton Institute'
+copyright = u'2022, Information & Computational Sciences, The James Hutton Institute'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
